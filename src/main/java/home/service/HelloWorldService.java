8c1e4047283f68e5025f57b28120fe17e1a6f101
vaspoz <vaspoz@mail.ru>
2018-02-17 13:01:54 +0100

    test

--- src/main/java/home/service/HelloWorldService.java
+++ src/main/java/home/service/HelloWorldService.java
@@ -10,7 +10,7 @@ import org.springframework.stereotype.Component;
 @Component
 public class HelloWorldService {
 
-    @Value("${name:World}")
+    @Value("${name:Worldddd}")
     private String name;
 
     public String getHelloMessage() {
